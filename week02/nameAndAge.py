32c40fe21cf69d6accdccc56bc9840e39d335383
nataliia-dragunova <nataliia.dragunova@gmail.com>
2024-02-10 00:25:21 +0000

    week 02

---
+++ week02/nameAndAge.py
@@ -0,0 +1,19 @@
+# nameAndAge.py
+# Program reads in a name and age and outputs info
+# author: Nataliia Dragunova
+'''
+name = input("please enter a name:")
+age = input("please enter an age:")
+print ("Hello " + name + ", your age is " + age +".");
+
+name = input("please enter a name:")
+age = input("please enter an age:")
+print ("Hello " + name + ",\nyour age is " + age +".");
+'''
+# math tasks
+'''
+answer = 21 - 4
+print (answer);
+'''
+answer = 2==3
+print (answer);
